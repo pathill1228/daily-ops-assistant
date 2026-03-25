82aa438ddb3e8c317b066c5b28f75d340a04a95a
pathill1 <pathill1228@gmail.com>
2026-03-25 14:44:18 -0500

    added chatGPT code for extras object

--- app/dailyData.tsx
+++ app/dailyData.tsx
@@ -2,13 +2,30 @@
 
 import { useState } from "react";
 
-
 export default function DailyData() {
+  const [inputValue, setInputValue] = useState("");
+  const [extras, setExtras] = useState<{ AssociateName?: string }>({});
 
+  function addExtra() {
+    setExtras({
+      AssociateName: inputValue
+    });
+  }
 
   return (
     <div>
-      <input type="text" className="bg-white mt-2 mb-2"></input>
+      <input
+        type="text"
+        className="border mt-2 mb-2"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <button className="border ml-2 pl-1 pr-1" onClick={addExtra}>
+        +
+      </button>
+
+      <pre>{JSON.stringify(extras, null, 2)}</pre>
     </div>
   );
 }
