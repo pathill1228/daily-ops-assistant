0794772107c8f7aa9d2b66296b71665001a25d16
pathill1 <pathill1228@gmail.com>
2026-03-24 19:48:03 -0500

    began dailyData.tsx

---
+++ app/dailyData.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
+
+export default function DailyData() {
+
+
+  return (
+    <div>
+      <input type="text" className="bg-white mt-2 mb-2"></input>
+    </div>
+  );
+}
