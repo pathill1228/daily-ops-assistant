6224899e8b715061a48ecc7ea0e5ae0d7fdcc030
pathill1 <pathill1228@gmail.com>
2026-04-21 20:34:32 -0500

    Added DisplayEmployeeData

--- app/dailyData.tsx
+++ app/dailyData.tsx
@@ -2,11 +2,22 @@
 
 import { useState } from "react";
 
+function DisplayEmployeeData({data}){
+  
+  if(!data) return <div></div>
+  else return(
+    //logic to display names after every entry
+  );
+}
+
 export default function DailyData() {
+
+  const [data, setData] = useState(null);
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter'){
-      console.log('Input Value: ', e.target.value);
+      //console.log('Input Value: ', e.target.value);
+      setData(e.target.value);
     }
   }
   
@@ -14,6 +25,7 @@ export default function DailyData() {
   return (
     <div>
       <input className="border p-1 mb-2" placeholder="Employee Name" onKeyDown={handleKeyDown}></input>
+      <DisplayEmployeeData data={data}/>
     </div>
   );
 } 
